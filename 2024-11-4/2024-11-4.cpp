74790a1515ae6845feaee5bd580f4dfb710c120d
lz <18172331625@163.com>
2024-11-04 15:02:38 +0800

    调试学习

---
+++ 2024-11-4/2024-11-4.cpp
@@ -0,0 +1,89 @@
+﻿//#include <iostream>
+//
+//int getValue()
+//{
+//std::cerr << "getValue() called\n";
+//    return 4;
+//}
+//
+//int main()
+//{
+//std::cerr << "main() called\n";
+//    std::cout << getValue << '\n';
+//
+//    return 0;
+//}
+
+//#include <iostream>
+//
+//void printValue(int value)
+//{
+//    std::cout << value << '\n';
+//}
+//
+//int main()
+//{
+//    printValue(5);
+//
+//    return 0;
+//}
+
+//#include <iostream>
+//
+//void printValue(int value)
+//{
+//    std::cout << value << '\n';
+//}
+//
+//int main()
+//{
+//    printValue(5);
+//    printValue(6);
+//    printValue(7);
+//
+//    return 0;
+//}
+
+//#include <iostream>
+//
+//int main()
+//{
+//	int x{ 1 };
+//	std::cout << x << ' ';
+//
+//	x = x + 2;
+//	std::cout << x << ' ';
+//
+//	x = x + 3;
+//	std::cout << x << ' ';
+//
+//	return 0;
+//}
+
+
+#include <iostream>
+
+void a()
+{
+	std::cout << "a() called\n";
+}
+
+void b()
+{
+	std::cout << "b() called\n";
+	a();
+}
+
+int main()
+{
+	a();
+	b();
+
+	return 0;
+}
+
+
+
+
+
+
