922d80b85969fcfe80350c5c8ad84a3c66cd84b9
lz <18172331625@163.com>
2024-11-13 17:08:12 +0800

    运算符

---
+++ 2024-11-13/2024-11-13.cpp
@@ -0,0 +1,86 @@
+﻿////#include <iostream>
+////
+////int getValue()
+////{
+////    std::cout << "Enter an integer: ";
+////
+////    int x{};
+////    std::cin >> x;
+////    return x;
+////}
+////
+////int main()
+////{
+////    std::cout << getValue() + (getValue() * getValue()) << '\n'; // a + (b * c)
+////    return 0;
+////}
+//
+//#include <cassert> // for assert
+//#include <cstdint> // for std::int64_t
+//#include <iostream>
+//
+//// note: exp 必须是非负整数
+//// note: 不执行越界检查
+//constexpr std::int64_t powint(std::int64_t base, int exp)
+//{
+//	assert(exp >= 0 && "powint: exp parameter has negative value");
+//
+//	// 处理 base 为 0 的情况
+//	if (base == 0)
+//		return (exp == 0) ? 1 : 0;
+//
+//	std::int64_t result{ 1 };
+//	while (exp > 0)
+//	{
+//		if (exp & 1)  // 如果exp 为奇数
+//			result *= base;
+//		exp /= 2;
+//		base *= base;
+//	}
+//
+//	return result;
+//}
+//
+//int main()
+//{
+//	std::cout << powint(7, 12) << '\n'; // 7 的 12 次方
+//
+//	return 0;
+//}
+//
+
+
+//#include <iostream>
+//
+//int main()
+//{
+//    int x{ 1 };
+//    int y{ 2 };
+//
+//    std::cout << (++x, ++y) << '\n'; // x与y分别加以, 计算的结果是右操作数
+//
+//    return 0;
+//}
+
+
+//#include <iostream>
+//
+//int main()
+//{
+//    double d1{ 100.0 - 99.99 }; // 数学上应该等于 0.01 
+//    double d2{ 10.0 - 9.99 }; // 数据上应该等于 0.01
+//
+//    if (d1 == d2)
+//        std::cout << "d1 == d2" << '\n';
+//    else if (d1 > d2)
+//        std::cout << "d1 > d2" << '\n'; 
+//    else if (d1 < d2)
+//        std::cout << "d1 < d2" << '\n';
+//
+//    return 0;
+//}
+
+//逻辑AND的优先级高于逻辑OR
+
+//短路求值
+
---
+++ 2024-11-9/2024-11-9.cpp
@@ -0,0 +1,127 @@
+﻿//#include <iostream>
+//
+//int main()
+//{
+//    std::cout << "Enter your age: ";
+//    int age{};
+//    std::cin >> age;
+//
+//    const int constAge{ age }; // 使用非常量的值初始化常变量
+//
+//    age = 5;      // ok: age 不是const, 所以可以改变值
+//    //constAge = 6; // error: constAge 是 const, 不能改变值
+//
+//    return 0;
+//}
+
+//c ? x : y	表达式c的结果为true，计算x，否则计算y
+
+
+//#include <iostream>
+//
+//int main()
+//{
+//    constexpr bool inBigClassroom{ false };
+//    constexpr int classSize{ inBigClassroom ? 30 : 20 };
+//    std::cout << "The class size is: " << classSize << '\n';
+//
+//    return 0;
+//}
+
+
+//#include <iostream>
+//
+//int main()
+//{
+//    int x{ 2 };
+//    int y{ 1 };
+//    int z{ 10 - x > y ? x : y };            // 先求值 2
+//    std::cout << z;
+//
+//    return 0;
+//}
+
+////操作数类型保持一致
+//#include <iostream>
+//
+//int main()
+//{
+//    std::cout << (true ? 1 : 2) << '\n';    // okay: 两个操作数的类型都是int
+//
+//    std::cout << (false ? 1 : 2.2) << '\n'; // okay: int  1 被转换为 double
+//
+//    std::cout << (true ? -1 : 2u) << '\n';  // 令人惊讶: -1 被转换为 unsigned int, 发生溢出 
+//
+//    return 0;
+//}
+
+//#include <iostream>
+//
+//constexpr int greater(int x, int y) // 是一个 constexpr 函数 可以在编译时计算，提高性能
+//{
+//    return (x > y ? x : y);
+//}
+//
+//int main()
+//{
+//    constexpr int x{ 5 };
+//    constexpr int y{ 6 };
+//
+//    // 稍后解释这里为啥使用变量
+//    constexpr int g{ greater(x, y) }; // 编译时计算
+//
+//    std::cout << g << " is greater!\n";
+//
+//    return 0;
+//}
+
+
+//#include <iostream>
+//#include <string>
+//
+//int main()
+//{
+//    std::cout << "Enter your full name: ";
+//    std::string name{};
+//    std::cin >> name; // 可能不一定按预期工作，因为std::cin读取到 空白字符，就会停止
+//
+//    std::cout << "Enter your favorite color: ";
+//    std::string color{};
+//    std::cin >> color;
+//
+//    std::cout << "Your name is " << name << " and your favorite color is " << color << '\n';
+//
+//    return 0;
+//}
+
+//#include <iostream>
+//#include <string> // 引入 std::string 和 std::getline
+//
+//int main()
+//{
+//    std::cout << "Enter your full name: ";
+//    std::string name{};
+//    std::getline(std::cin >> std::ws, name); // 将一整行输入读取到 name
+//
+//    std::cout << "Enter your favorite color: ";
+//    std::string color{};
+//    std::getline(std::cin >> std::ws, color); // 将一整行输入读取到 color
+//
+//    std::cout << "Your name is " << name << " and your favorite color is " << color << '\n';
+//
+//    return 0;
+//}
+
+// 获取字符串长度
+#include<iostream>
+#include<string>
+
+int main()
+{
+	std::string name{ "aLEX"};
+	std::cout << name.length() << "\n";
+
+	return 0;
+
+}
+
